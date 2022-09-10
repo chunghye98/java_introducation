959bb0e0662c1077a5081786bb84f79a3bad496e
chunghye98 <57451700+chunghye98@users.noreply.github.com>
2022-09-10 23:50:00 +0900

    part6 init commit

---
+++ src/part6/MethodOverloadExam.java
@@ -0,0 +1,10 @@
+package part6;
+
+public class MethodOverloadExam {
+    public static void main(String[] args) {
+        MyClass2 m = new MyClass2();
+        System.out.println(m.plus(4, 5));
+        System.out.println(m.plus(4, 6, 7));
+        System.out.println(m.plus("hello","world"));
+    }
+}
